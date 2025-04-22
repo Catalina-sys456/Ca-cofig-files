33abe1ae472db67038db209f3a71e49b89e00e41
sccuhha <aaaaaa.com>
2025-04-22 12:32:31 +0000

    emacs

--- .emacs.d/python.el
+++ .emacs.d/python.el
@@ -1,1 +1,24 @@
-/home/ghost/.emacs.d/python.el
+;;;python language
+
+(use-package python
+   :defer t
+   :mode ("\\.py\\'" . python-mode)
+   :interpreter ("python3" . python-mode)
+   :config
+   ;;;for debug
+  (require 'dap-python)
+)
+
+(use-package pyvenv
+   :ensure t
+   :config
+   (pyvenv-mode t)
+  )
+
+  (use-package lsp-pyright
+   :ensure t
+   :config
+   :hook
+   (python-mode . (lambda ()
+    (require 'lsp-pyright)
+    (lsp-deferred))))
--- .emacs.d/rust.el
+++ .emacs.d/rust.el
@@ -1,1 +1,25 @@
-/home/ghost/.emacs.d/rust.el
+;;;rust language
+
+ (use-package rust-mode
+ :ensure t
+ :functions dap-register-debug-template
+ :bind
+ ("C-c C-c" . rust-run)
+ :hook
+ (rust-mode . lsp-deferred)
+ :config
+ ;; debug
+ (require 'dap-gdb)
+ (dap-register-debug-template "Rust::LLDB Run Configuration"
+                (list :type "lldb"
+     :request "launch"
+        :name "rust-lldb::Run"
+     :gdbpath "rust-lldb"
+     :target nil
+     :cwd nil)))
+
+(use-package cargo
+ :ensure t
+ :hook
+ (rust-mode . cargo-minor-mode))
+ 
